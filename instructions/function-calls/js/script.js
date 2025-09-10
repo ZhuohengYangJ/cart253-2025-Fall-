7550747513a53c5256ff26a4379fc4e537297715
ZhuohengYangJ <jasonzhuohengyang@gmail.com>
2025-09-10 19:38:26 -0400

    function calls instruction

---
+++ instructions/function-calls/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Sun
+ * Zhuoheng Yang
+ * 
+ * 
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**creates canvas for the project */
+function setup() {
+    createCanvas(700, 500);
+
+}
+
+
+/**
+ draw background and objects
+*/
+function draw() {
+    //set up blue sky
+    background(173, 216, 230);
+    //draw a red sun
+    fill(200, 20, 20);
+    ellipse(50, 50, 50,50);
+
+}
