800f1ef47c54236a8c96483ff7a9dce8ed5d7a16
ZhuohengYangJ <jasonzhuohengyang@gmail.com>
2025-09-10 21:05:31 -0400

    Started the instructions challenge

--- instructions/instruction-challenge/js/script.js
+++ instructions/instruction-challenge/js/script.js
@@ -23,7 +23,7 @@ function setup() {
 function draw() {
     //draw light bku sky as background
     background(173, 216, 230);
-    //draw a red sun
+    //draw a red sun with orange stroke
     push();
     stroke(255, 165, 0);
     strokeWeight(3);
@@ -38,6 +38,20 @@ function draw() {
     // cover the bottom half of the green circle with dark bluee sea water to make it look more like a semicircle island
     fill(0, 0, 139);
     rect(0, 400, 800, 400);
+
+    //light yellow bird body
+    fill(255, 255, 197);
+    rect(600, 90, 50, 30);
+
+    //draw bird wing with trangle
+    fill(255, 255, 197);
+    triangle(610, 90, 640, 90, 640, 65);
+
+    //drawing beak
+    fill(255, 255, 197);
+    triangle(580, 110, 600, 100, 600, 80);
+
+
 
 
 }
