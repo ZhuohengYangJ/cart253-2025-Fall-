870bd18b1f97154171924586f37f1869fcfb25b6
ZhuohengYangJ <jasonzhuohengyang@gmail.com>
2025-09-10 20:45:58 -0400

    Started the instructions challenge

--- instructions/instruction-challenge/js/script.js
+++ instructions/instruction-challenge/js/script.js
@@ -1,17 +1,18 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing Practice
+ * Zhuohenvg Yang
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Drawing a masterpiece
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED! (Please don't)
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+create canvas
 */
 function setup() {
+    createCanvas(800, 800);
 
 }
 
@@ -20,5 +21,23 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    //draw light bku sky as background
+    background(173, 216, 230);
+    //draw a red sun
+    push();
+    stroke(255, 165, 0);
+    strokeWeight(3);
+    fill(200, 0, 0);
+    ellipse(70, 70, 70,70);
+    pop();
+
+    //draw a greeen circl represents a small island covered with trees
+    fill(0, 150, 10);
+    ellipse(120, 400, 250, 400);
+
+    // cover the bottom half of the green circle with dark bluee sea water to make it look more like a semicircle island
+    fill(0, 0, 139);
+    rect(0, 400, 800, 400);
+
 
 }
