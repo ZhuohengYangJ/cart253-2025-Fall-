e21057d57f16fb1500c6f08a41829f35cc538f91
ZhuohengYangJ <jasonzhuohengyang@gmail.com>
2025-09-17 20:13:07 -0400

    variables movement practice

---
+++ topics/movement/js/script.js
@@ -0,0 +1,61 @@
+/**
+ * Title of Project
+ * Zhuoheng Yang
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+let bird = { 
+    x: 120,
+    y: 400,
+    size: 50,
+    velocity: {
+        x: 1,
+        y: -2
+    },
+    minVelocity:{
+        x: -3,
+        y: -2
+    },
+    maxVelocity:{
+        x: 3,
+        y: 2
+    },
+    acceleration: {
+        x: 0.025,
+        y: -0.05
+    }
+
+};
+//create canvas
+function setup() {
+    createCanvas(640, 480);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0);
+
+    //move the bird
+    bird.velocity.x = bird.velocity.x + bird.acceleration.x;
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+    
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+
+    //draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+
+}
